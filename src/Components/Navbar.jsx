b492710ce893bdf7a9bb9be06d6774285458fadf
rabab003 <bintaliraq4@gmail.com>
2025-04-04 12:10:51 +0300

    12:10pm

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
+        <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
+            {/* logo */}
+            {/* desktop navigation */}
+            {/* get in touch btn */}
+            {/* mobile menu */}
+
+        </div>
+
+    </nav>
+  )
+}
