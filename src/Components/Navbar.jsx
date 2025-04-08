47fe415186faa20faef36f6d70c0460bb044459e
rabab003 <bintaliraq4@gmail.com>
2025-04-08 23:18:01 +0300

    11:17 pm

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [activeLink, setActiveLink] = useState("#home")
+
+  const navLinks = [
+    {href: "#home", label:"Home"},
+    {href: "#about", label:"About Us"},
+    {href: "#services", label:"Our Services"},
+    {href: "#testimonials", label:"Testimonials"},
+  ]
   return (
     <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
         <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
             {/* logo */}
             <div className='flex items-center gap-1 cursor-pointer'>
-                <div className='w-4 h-4 bg-blue-600 rounded-full opacity-100 hover:opacity-75'></div>
-                <div className='w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100 hover:opacity-75'></div>
+                <div className='w-4 h-4 bg-blue-600 rounded-full opacity-100 hover:opacity-75 transition-opacity'></div>
+                <div className='w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100 hover:opacity-75 transition-opacity'></div>
             </div>
             {/* desktop navigation */}
             <div>
-              <li>Home</li>
-              <li>About</li>
-              <li>Services</li>
-              <li>Contact us</li>
+
+              <div>{
+                navLinks.map((link, index)=>(
+                  <a className="text-sm font-medium relative"
+ key={index} 
+                  href={link.href}>
+                    {link.label}
+                    </a>
+                ))
+              }</div>
+        
             </div>
             {/* get in touch btn */}
             {/* mobile menu */}
